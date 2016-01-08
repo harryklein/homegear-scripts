66124fc8aedb2434ec24f9d0394678bb2d62157b
Harry Klein <harry.klein@joomla-hklein.de>
2016-01-08 01:22:56 +0100

    Schalter --id, --list, --address und --help hinzugefügt

--- scripts/getAllInfo.php
+++ scripts/getAllInfo.php
@@ -1,12 +1,59 @@
 #!/usr/bin/env php
 <?php
+
+	function usage(){
+?>
+Aufruf [--id ID]|[--address ADDRESS] | [--list] | [--help]
+	--list           : Listet alle bekannten Geräte mit Id, Name, Type, Firmware und Name auf
+	--id ID          : Ausgabe aller Details eines Gerätes mit der Id ID
+	--address ADDRESS: Ausgabe aller Details eines Gerätes mit der Adresse ADDRESS
+	(ohne)           : Ausgabe Details aller bekannten Geräte
+	--help           : Diese Hilfe
+
+<?php
+
+	}
+	$ID=0;
+	$list=0;
+	$address="";
 	switch ($argv[1]) {
 		case "--list":
 			$list=1;
 			break;
+		case "--id":
+			$ID=$argv[2];
+			break;
+		case "--address":
+			$address=$argv[2];
+                        break;
+		case "--help":
+			usage();
+			exit(0);
+			break;
+
 	}
 	include_once("Connect.php");
 	$data = $Client->send("listDevices", array());
+	if ( $ID > 0 || $address != "" ) {
+		$foundId = false;
+		foreach ( $data as $item ){
+			if( empty($item["PARENT"])){
+				if ( ($item["ID"] == $ID) || ($address == $item["ADDRESS"]) ) {
+					$address = $item["ADDRESS"];
+					$foundId = true;
+				}
+
+			}
+		}
+		if (!$foundId) {
+			echo "Id [$ID] nicht gefunden. Abbruch\n";
+			exit(1);
+		}
+		$data = array();
+		$data[] = $Client->send("getDeviceDescription", array($address));
+
+	}
+
 	// print_r($data);
 	
 	foreach ( $data as $item ){
@@ -16,6 +63,8 @@
 			echo "Address .. : " . $item["ADDRESS"] . "\n";
 			echo "Type ..... : " . $item["TYPE"]  . "\n";
 			echo "Firmware . : " . $item["FIRMWARE"]  . "\n";
+			$deviceInfo = $Client->send("getDeviceInfo", array($item["ID"]));
+	                echo "Name ..... : " . utf8_encode($deviceInfo["NAME"]). "\n";
 			if ($list == 1){
 				continue;
 			}
@@ -38,9 +87,6 @@
                                         printf  ("- %-25s : %s\n", $key,$paramSet[$key]);
                                 }
 			}
-			$deviceInfo = $Client->send("getDeviceInfo", array($item["ID"]));
-			echo "Name .....: " . utf8_encode($deviceInfo["NAME"]). "\n";
-			print_r($deviceInfo);
 		}
 
 	}
