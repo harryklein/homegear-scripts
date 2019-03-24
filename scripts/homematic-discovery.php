4a2bebf443cec28124846989a464666924a5ffbf
Harry Klein <harry@joomla-hklein.de>
2019-03-24 20:34:39 +0100

    Code formatiert

--- scripts/homematic-discovery.php
+++ scripts/homematic-discovery.php
@@ -1,56 +1,58 @@
 #!/usr/bin/env php
 <?php
-	include_once("/var/lib/homegear/scripts/Connect.php");
+include_once ("/var/lib/homegear/scripts/Connect.php");
 
-	function usage(&$argv){
-		echo "Aufruf $argv[0] HM-ES-PMSw1-Pl|HM-WDS10-TH-O|HM-CC-TC|HM-Sec-SC|all\n";
-	}
+function usage(&$argv)
+{
+    echo "Aufruf $argv[0] HM-ES-PMSw1-Pl|HM-WDS10-TH-O|HM-CC-TC|HM-Sec-SC|all\n";
+}
 
-	if (count($argv) == 1 ){
-		usage($argv);
-		exit(1);
-	}
-	$allDevices = false;
+if (count($argv) == 1) {
+    usage($argv);
+    exit(1);
+}
+$allDevices = false;
 
-	switch ($argv[1]) {
-		case "HM-ES-PMSw1-Pl":
-		case "HM-WDS10-TH-O":
-		case "HM-CC-TC":
-		case "HM-Sec-SC":
-			$deviceType=$argv[1];
-			break;
-		case "all":
-			$allDevices = true;
-			break;
-		default:
-			usage($argv);
-			exit(1);
-	}
+switch ($argv[1]) {
+    case "HM-ES-PMSw1-Pl":
+    case "HM-WDS10-TH-O":
+    case "HM-CC-TC":
+    case "HM-Sec-SC":
+        $deviceType = $argv[1];
+        break;
+    case "all":
+        $allDevices = true;
+        break;
+    default:
+        usage($argv);
+        exit(1);
+}
 
-	$data = $Client->send("listDevices", array());
-	// print_r($data);
+$data = $Client->send("listDevices", array());
+// print_r($data);
 
-	$result = array();
+$result = array();
 
-	foreach ( $data as $item ){
-		if( empty($item["PARENT"])){
-			if ( ($allDevices === true) || ($item["TYPE"] == $deviceType)) {
-				$i = array();
-				$i['{#ADDRESS}'] = $item["ADDRESS"];
-				$i['{#ID}'] = $item["ID"];
-				$i['{#TYPE}'] = $item["TYPE"];
-				$deviceInfo = $Client->send("getDeviceInfo", array($item["ID"]));
-				$name = $deviceInfo["NAME"];
-				$name = utf8_encode($name);
-				$name = str_replace('"','',$name);
-				$i['{#NAME}'] = $name;
-				$result[] = $i;
-
-			}
-		}
-	}
-	$resultList = array();
-	$resultList['data'] = $result;
-	echo json_encode($resultList);
-	echo "\n";
+foreach ($data as $item) {
+    if (empty($item["PARENT"])) {
+        if (($allDevices === true) || ($item["TYPE"] == $deviceType)) {
+            $i = array();
+            $i['{#ADDRESS}'] = $item["ADDRESS"];
+            $i['{#ID}'] = $item["ID"];
+            $i['{#TYPE}'] = $item["TYPE"];
+            $deviceInfo = $Client->send("getDeviceInfo", array(
+                $item["ID"]
+            ));
+            $name = $deviceInfo["NAME"];
+            $name = utf8_encode($name);
+            $name = str_replace('"', '', $name);
+            $i['{#NAME}'] = $name;
+            $result[] = $i;
+        }
+    }
+}
+$resultList = array();
+$resultList['data'] = $result;
+echo json_encode($resultList);
+echo "\n";
 ?>
